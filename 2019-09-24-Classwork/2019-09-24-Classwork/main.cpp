67988fb05f28c6517cf7f90c13570ff6c0bfdae3
RKhalilKelly <55443399+RKhalilKelly@users.noreply.github.com>
2019-09-24 08:40:04 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,26 @@
+// Robert Khalil Kelly
+// Intro to Computer Science
+// Mr. Williams
+// Period 1
+// 2019-09-24-Classwork
+ 
+#include <iostream>
+
+int main()
+{
+	int x = 1;
+	std::cout << x << std::endl;
+
+	int y = 2;
+	std::cout << x << std::endl;
+
+	int a = 3;
+	std::cout << x << std::endl;
+
+	int b = 4;
+	std::cout << x << std::endl;
+
+	int o = 5;
+	std::cout << x << std::endl;
+
+}
