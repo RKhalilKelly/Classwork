a6959106b8614794c379240a61e9660d3a7fded9
RKhalilKelly <55443399+RKhalilKelly@users.noreply.github.com>
2019-10-02 08:41:35 -0400

    Add files via upload

---
+++ 2019-10-02-Classwork/2019-10-02-Classwork/main.cpp
@@ -0,0 +1,28 @@
+// Robert Khalil Kelly
+// Intro to Computer Science
+// Mr. Williams
+// Period 1
+// 2019-10-02-Classwork
+
+#include <iostream>
+
+int main()
+{
+	char letterA = 'A';
+	char b = 'h';
+	char light = '24';
+	char Khalil = '45';
+	
+	short W = 1;
+	int Periods = 8;
+	long long botbot = -9223372036;
+
+	bool thing = true;
+	bool myNameIsKhalil = true;
+
+	float three = 3;
+	double hi = 308;
+	float good = 47;
+}
+
+
