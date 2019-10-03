6b3dacc59c07ce61ae255d3a19460218fb8ab261
RKhalilKelly <55443399+RKhalilKelly@users.noreply.github.com>
2019-10-03 08:39:02 -0400

    Add files via upload

---
+++ 2019-10-03-Classwork/2019-10-03-Classwork/main.cpp
@@ -0,0 +1,16 @@
+// Robert Khalil Kelly
+// Intro to Computer Science
+// Period 1
+// Mr.Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "Enter you favorite number: ";
+	int favoriteNumber;
+	std::cin >> favoriteNumber;
+	std::cout << "cool, " << favoriteNumber << "is my favorite number too!" << std::endl;
+}
